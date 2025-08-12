5562effc90b831e0a1cd3da21d7c6356769582b6
dimitkos <dimitkos@yahoo.gr>
2025-08-12 20:08:51 +0300

    Renames, add folder structure

---
+++ SeasonEmployees/Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SeasonEmployees.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+       
+    }
+}
